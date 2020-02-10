fbe7db22d07f9ef4736d4336482e60878dfdd715
robert <r.vanderstarre@solcon.nl>
2020-02-10 20:38:06 +0100

    split up unit tests, updated pipeline

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,17 @@
 
 class TestData:
 
+    class Player:
+
+        def __init__(self, name):
+            self.name = name
+            self.scoreblock = self.ScoreBlock()
+
+        class ScoreBlock:
+
+            def total_score(self):
+                return 10
+
     class Round:
 
         def __init__(self, *args):
